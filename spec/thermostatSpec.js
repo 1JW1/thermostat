05572a548543d365f83d28cc70d76b33c3828b85
Jerome Wanliss <j.wanliss@yahoo.co.uk>
2021-01-26 11:21:56 +0000

    business logic spec passed

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,15 +13,57 @@ describe("thermostat", function() {
       thermostat.increase(5)
       expect(thermostat.temp()).toBe(25);
       });
-
-      it("You can decrease the temperature with a down function", function() {
+      
+    it("You can decrease the temperature with a down function", function() {
         thermostat.decrease(5)
         expect(thermostat.temp()).toBe(15);
         });
+        
+    it("The minimum temperature is 10 degrees", function() {
+        expect(function() {thermostat.decrease(11)}).toThrow(new Error("No less than 10 degrees!"));
+        });
 
-        it("The minimum temperature is 10 degrees", function() {
-            expect(function() {thermostat.decrease(11)}).toThrow(new Error("No less than 10!"));
+        it("If power saving mode is on, the maximum temperature is 25 degrees", function() {
+            expect(function() {thermostat.increase(6)}).toThrow(new Error("Max temp is 25 degrees in power save mode!"));
             });
 
+            it("If power saving mode is off, the maximum temperature is 32 degrees", function() {
+                return this.powersave = false;
+                expect(function() {thermostat.increase(13)}).toThrow(new Error("Max temp is 32 degrees in power save mode!"));
+                });
+
+                it("Power saving mode is on by default but it can also be turned off", function() {
+                    thermostat.powerModeOff()
+                    expect(thermostat.powersave).toBe(false);
+                    });
+
+                    it("You can reset the temperature to 20 with a reset function", function() {
+                        thermostat.reset()
+                        expect(thermostat.temperature).toBe(20);
+                        });
+
+                        it("You can ask about the thermostat's current energy usage", function() {
+                            return this.powersave = false;
+                            
+                            thermostat.decrease(3)
+                            thermostat.usage()
+                            thermostat.increase(8)
+                            thermostat.usage()
+                            thermostat.increase(1)
+
+                            expect(thermostat.temperature = 17).toBe("Low-usage.");
+                            expect(thermostat.temperature = 25).toBe("Medium-usage.");
+                            expect(thermostat.temperature = 26).toBe("High-usage.");
+                            });
+
+                            it("increases the value of temperature by 1", function() {
+                                thermostat.increase()
+                                expect(thermostat.temperature).toBe(21);
+                                });
+
+                                it("decreases the value of temperature by 1", function() {
+                                    thermostat.decrease()
+                                    expect(thermostat.temperature).toBe(19);
+                                    });
 
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,20 +1,42 @@
 class Thermostat {
     constructor() {
-        this.temperature = 20
+        this.temperature = 20,
+        this.powersave = true
     }
 
    temp() { 
        return this.temperature
     }
 
-    increase(number) {
+    increase(number = 1) {
         this.temperature += number
-    }
+        if (this.powersave = true && this.temperature > 25) {
+            throw new Error("Max temp is 25 degrees in power save mode!")
+        } else if (this.powersave = false && this.temperature > 32) {
+            throw new Error("Max temp is 32 degrees in power save mode!") }
+        }
 
-    decrease(number) {
+    decrease(number = 1) {
         this.temperature -= number
         if (this.temperature < 10) {
-        throw new Error("No less than 10!")
+        throw new Error("No less than 10 degrees!")
         }
+    }
+
+    powerModeOff() {
+        return this.powersave = false;
+    }
+
+    reset() {
+        return this.temperature = 20;
+    }
+
+    usage() {
+        if (this.temperature < 18) {
+            "Low-usage."
+        } else if (this.temperature <= 25) {
+            "Medium-usage."
+        } else (this.temperature >= 26 & this.powersave == false)
+            "High-usage."
     }
 } 
